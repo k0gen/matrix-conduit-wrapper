7bb351485cf7d05c93e0f9ecdc6039506c211dee
Mariusz Kogen <k0gen@pm.me>
2021-04-08 01:33:09 +0000

    Initial conduit config + lan

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
-
+echo "[global]" > /root/conduit.toml
+echo "server_name = \"${TOR_ADDRESS}\"" >> /root/conduit.toml
+echo "database_path = \"/root/conduit_db\"" >> /root/conduit.toml
+echo "port = 6167" >> /root/conduit.toml
+echo "max_request_size = 20_000_000" >> /root/conduit.toml
+echo "allow_registration = false" >> /root/conduit.toml
+echo "allow_encryption = true" >> /root/conduit.toml
+echo "allow_federation = true" >> /root/conduit.toml
+echo "address = \"127.0.0.1\"" >> /root/conduit.toml
+export CONDUIT_CONFIG="conduit.toml"
 exec tini conduit
 
