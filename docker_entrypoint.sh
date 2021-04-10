33498af2b023d23efda041f99700d6886ef28d1a
Mariusz Kogen <k0gen@pm.me>
2021-04-10 21:51:55 +0000

    nginx reverse proxy config

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -10,6 +10,22 @@ echo "allow_registration = true" >> /root/conduit.toml
 echo "allow_encryption = true" >> /root/conduit.toml
 echo "allow_federation = true" >> /root/conduit.toml
 echo "address = \"127.0.0.1\"" >> /root/conduit.toml
+
+cat <<EOT >> /etc/nginx/nginx.conf
+
+server {
+    listen 443;
+    listen 8448;
+EOT
+echo "    server_name ${TOR_ADDRESS};" >> /etc/nginx/nginx.conf
+cat <<EOT >> /etc/nginx/nginx.conf
+
+    location /_matrix/ {
+        proxy_pass http://localhost:6167/_matrix/;
+    }
+}
+EOT
+
 export CONDUIT_CONFIG="conduit.toml"
 exec tini conduit
 
